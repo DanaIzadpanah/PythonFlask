ad77ee5c9c704f3e18d308eabf5312786e0ce392
Dana Izadpanah <danaizad8624@gmail.com>
2023-12-24 14:39:36 -0600

    RestAPI

--- hello.py
+++ hello.py
@@ -5,5 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/hello")
 def hello_world():
     return "Hellow universe"
+
+if __name__ == '__main__':
+    app.run(debug=True)
