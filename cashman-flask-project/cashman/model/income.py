92fca8c3cecda93bdb92a99db2cdbca6b37f6978
Dana Izadpanah <danaizad8624@gmail.com>
2023-12-24 13:12:19 -0600

    Fixing the bugs with the files

--- cashman-flask-project/cashman/model/income.py
+++ cashman-flask-project/cashman/model/income.py
@@ -3,7 +3,8 @@ from marshmallow import post_load
 from .transaction import Transaction, TransactionSchema
 from .transaction_type import TransactionType
 
-class income(Transaction):
+
+class Income(Transaction):
     def __init__(self, description, amount):
         super(Income, self).__init__(description, amount, TransactionType.INCOME)
 
